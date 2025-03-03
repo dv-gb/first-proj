f4b272505691baa8b95fa83e8d6b79251d6a24d8
Gabe <gabguban7777@gmail.com>
2025-03-03 19:47:43 +0800

    fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import ClientDashboard from "./components/ClientDashboard";
 import LoginPage from "./components/LoginPage";
 import RegisterPage from "./components/RegisterPage";
