4ca47576cf8b9718f2ce557a22205e8d99e4c091
Gabe <gabguban7777@gmail.com>
2025-03-03 22:50:24 +0800

    last na to

--- client/src/components/ClientDashboard.jsx
+++ client/src/components/ClientDashboard.jsx
@@ -47,25 +47,27 @@ export default function Dashboard() {
   return (
     <div className="flex flex-col justify-center items-center w-full h-screen">
       <h1 className="text-2xl font-bold mb-4">Welcome to Dashboard</h1>
+      {user && (
+        <div className="bg-gray-100 p-4 rounded-lg shadow-md w-[250px]">
+          <h2 className="text-xl font-semibold">User Information</h2>
+          <p>
+            <strong>First Name:</strong> {user.firstname}
+          </p>
+          <p>
+            <strong>Last Name:</strong> {user.lastname}
+          </p>
+          <p>
+            <strong>Username:</strong> {user.username}
+          </p>
+          <p>
+            <strong>Contact Number:</strong> {user.contact}
+          </p>
+          <p>
+            <strong>Email:</strong> {user.email}
+          </p>
+        </div>
+      )}
 
-      <div className="bg-gray-100 p-4 rounded-lg shadow-md w-[250px]">
-        <h2 className="text-xl font-semibold">User Information</h2>
-        <p>
-          <strong>First Name:</strong> {user.firstname}
-        </p>
-        <p>
-          <strong>Last Name:</strong> {user.lastname}
-        </p>
-        <p>
-          <strong>Username:</strong> {user.username}
-        </p>
-        <p>
-          <strong>Contact Number:</strong> {user.contact}
-        </p>
-        <p>
-          <strong>Email:</strong> {user.email}
-        </p>
-      </div>
       <button
         onClick={handleLogout}
         className="mt-4 bg-gray-500 text-white px-4 py-2 rounded-xl hover:bg-gray-700"
